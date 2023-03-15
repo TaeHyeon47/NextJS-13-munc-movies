e8779d26fc95cd9da1d5ed8e98587f9820899be2
Tae Hyeon Jo <hillskuti@naver.com>
2023-03-15 20:25:30 +0900

    first commit

---
+++ src/app/head.jsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>munc Movies</title>
+      <meta content='width=device-width, initial-scale=1' name='viewport' />
+      <meta name='description' content='This is the munc Movies Service' />
+      <link rel='icon' href='/favicon.ico' />
+    </>
+  );
+}
