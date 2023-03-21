e1aea4dcea58dcd5f17502a445c2888295b2da34
Tae Hyeon Jo <hillskuti@naver.com>
2023-03-21 21:30:50 +0900

    오픈 API 활용 서버사이드 Data Fetch

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,34 @@
-import Image from 'next/image';
-import { Inter } from 'next/font/google';
+import Results from '@/components/Results';
 
-const inter = Inter({ subsets: ['latin'] });
+const API_KEY = process.env.API_KEY;
 
-export default function Home() {
-  return <h1 className='text-red-400'>HOME</h1>;
+// We don't use useEffect or any other hooks to get the data because this is going to be client side.
+
+// Server Side fetch data
+// async는 React component에서 불가, 오직 NextJS 13에서만 가능.
+export default async function Home({ searchParams }) {
+  const genre = searchParams.genre || 'fetchTrending';
+
+  console.log(genre);
+
+  const res = await fetch(
+    `https://api.themoviedb.org/3/${
+      genre === 'fetchTopRated' ? 'movie/top_rated' : 'trending/all/week'
+    }?api_key=${API_KEY}&language=ko-KR&page=1`,
+    { next: { revalidate: 10000 } }
+  );
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
+
+  const data = await res.json();
+
+  const results = data.results;
+
+  return (
+    <div>
+      <Results results={results} />
+    </div>
+  );
 }
