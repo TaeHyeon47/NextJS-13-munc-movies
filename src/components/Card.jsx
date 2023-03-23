fe398b303f8b5cc2b553b7c187c7967fe1182258
Tae Hyeon Jo <hillskuti@naver.com>
2023-03-23 21:35:14 +0900

    카드 컴포넌트 추가 1/2

---
+++ src/components/Card.jsx
@@ -0,0 +1,30 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+
+export default function Card({ result }) {
+  return (
+    <div className='cursor-pointer sm:p-3 sm:hover:shadow-slate-400 sm:shadow-md rounded sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-200 group'>
+      <Link href={`/movie/${result.id}`}>
+        <Image
+          src={`https://image.tmdb.org/t/p/original/${
+            result.backdrop_path || result.poster_path
+          }`}
+          width={500}
+          height={300}
+          className='sm:rounded-t-lg group-hover:opacity-80 transition-opacity duration-200'
+          style={{
+            maxWidth: '100%',
+            height: 'auto',
+          }}
+          placeholder='blur'
+          blurDataURL='/spinner.svg'
+          alt='image is not available'
+        ></Image>
+        <div className='p-2'>
+          <p>{result.overview}</p>
+        </div>
+      </Link>
+    </div>
+  );
+}
